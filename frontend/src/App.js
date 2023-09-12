0487577eae9c7895bacd02241c01a33d9ec2899f
LeonTGC <leon.cooper@gceducationandskills.ac.uk>
2023-09-12 11:05:20 +0100

    connected Main css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,10 @@ import AddTodo from "./pages/AddTodo"
 import Home from "./pages/Home"
 import EditTodo from "./pages/EditTodo"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import './css/Main.css'
 const App = () => {
   return (
-    <div>
+    <div id='main'>
       <BrowserRouter>
         <h1>Todo List</h1>
         <Navbar />
